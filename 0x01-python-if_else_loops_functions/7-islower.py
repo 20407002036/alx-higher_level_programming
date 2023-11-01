622b9306b124ca46b6f2587e4cec06ec96708595
Solomon <kyobenimaru001@gmail.com>
2023-11-01 15:57:24 +0300

    commit for the file 7-main.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin.python3
+def islower(c): #This is the prototype Given
+    #The function will be called from the main method,
+    #Then if input is C it will return true else false
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
