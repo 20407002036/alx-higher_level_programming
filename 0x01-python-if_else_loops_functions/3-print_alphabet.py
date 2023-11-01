29cb8d6ab004bb0308dc29a41ada5c1d75211933
Solomon <kyobenimaru001@gmail.com>
2023-11-01 10:04:22 +0300

    Commit for the file 6-print_comb3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin.python3
+output = ''.join(chr(char_code) for char_code in range(ord('a'), ord('z') + 1))
+ if (char_code != 'e' && char_code != 'q')
+  print('{}'.format(output), end='')
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for x in range(99):
+    
+    print('{} = 0x{:X}'.format(x,x))
