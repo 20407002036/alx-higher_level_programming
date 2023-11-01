e0bf2a4bbfbd5805b7e665fb4d632373bbc318b2
Solomon <kyobenimaru001@gmail.com>
2023-11-01 16:29:24 +0300

    commit for the file 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+     #   if number < 0:
+     #   last = number % -10
+     #   elif number >= 0:
+     #   last = number % 10
+
+     #   print(last)
+    last = abs(number) % 10
+    print(last)
+    return last
