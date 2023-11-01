2ef94f4975bc332b7329a5495c4417981c518bdc
Solomon <kyobenimaru001@gmail.com>
2023-11-01 16:43:04 +0300

    commit for the add function

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-     #   if number < 0:
-     #   last = number % -10
-     #   elif number >= 0:
-     #   last = number % 10
-
-     #   print(last)
     last = abs(number) % 10
     print("{}".format(last), end="")
     return last
