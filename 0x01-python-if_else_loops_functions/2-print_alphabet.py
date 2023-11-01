02aff211dd3d8cd1e6686141f98aa1048ce7bf28
Solomon <kyobenimaru001@gmail.com>
2023-11-01 08:56:11 +0300

    commit for the 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(''.join(chr(char_code) for char_code in range(ord('a'), ord('z') + 1)), end='')
+output = ''.join(chr(char_code) for char_code in range(ord('a'), ord('z') + 1))
+print('{}'.format(output), end='')
+
