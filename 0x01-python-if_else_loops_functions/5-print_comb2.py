a07ad9dfa8acc394c319d1777d6aa32108b94e5a
Solomon <kyobenimaru001@gmail.com>
2023-11-01 15:33:11 +0300

    commit for the file 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(100):
     if x != 99:
-        print("{:02}, ".format(x),end='')
+        print("{:02}, ".format(x), end='')
     else:
         print('{:02}'.format(x))
