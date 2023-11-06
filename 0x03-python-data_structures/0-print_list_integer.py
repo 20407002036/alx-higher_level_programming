4d09cd18e63d876cfd90366f18f85a4c072815a4
Solomon <kyobenimaru001@gmail.com>
2023-11-06 17:18:57 +0300

    commit for the file 0-main.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@ def print_list_integer(my_list=[]):
     #prints a list of given integers
     
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+        print("{:}".format(my_list[i]))
