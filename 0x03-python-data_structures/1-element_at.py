84fbe48c62b9cc4a72d9f8b87f38a846240f8993
Solomon <kyobenimaru001@gmail.com>
2023-11-06 09:12:39 +0300

    commit for the files corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return 0
-    elif idx > (len(my_list) - 1):
-        return 0
+    #if idx < 0:
+    #    return 
+    #elif idx > (len(my_list) - 1):
+    #   return
+
+    #this could have been done using one statement
+    list_len = len(my_list)
+    if idx >= list_len or idx < 0:
+        return
     else:
         return my_list[idx]
