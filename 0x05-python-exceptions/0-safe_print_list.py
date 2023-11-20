01214176b5697b86f9fac93dd9a07791276dce3e
Solomon <kyobenimaru001@gmail.com>
2023-11-20 12:32:11 +0300

    commit for the files 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end=' ')
+            # The print is quite important. have the placo holder in place
             printed_elements += 1
 
     except IndexError:
