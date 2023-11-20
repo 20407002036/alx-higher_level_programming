8f909381eb07a086608277f4dfc4b3c0b9d14344
Solomon <kyobenimaru001@gmail.com>
2023-11-20 13:03:56 +0300

    commit for the 3 files

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    printed = 0
+    try:
+        for i in range(x):
+
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+
+                printed+=1
+
+        return printed
+
+    except IndexError as e:
+        raise
+
+    finally:
+        print()
