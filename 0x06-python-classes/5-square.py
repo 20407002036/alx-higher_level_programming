e07d1c8c811ebbb5dc5012453d9a58617ccc2c4a
Solomon <kyobenimaru001@gmail.com>
2023-11-22 17:46:39 +0300

    commit for the file 5-square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,61 +1,39 @@
 #!/usr/bin/python3
-"""Class Definition"""
+"""Define a class Square."""
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    """
-    def __init__(self, size=0):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
         """
-        initialization function for our square class
-        """
-        if self.__validate_size(size):
-            self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """
-        getter for the size attribute
-        """
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """
-        setter for the size attribute
-        """
-        if self.__validate_size(value):
-            self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        """
-        calculates the area of the square
-        """
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """
-        prints the square using '#' characters
-        Use the for loops with the max values as the
-        self.__size
-        """
-        i = 0
+        """Print the square with the # character."""
         for i in range(0, self.__size):
-            j = 0
-            for j in range(0, self.__size):
-                print("#", end='')
-            print()
-
-    def __validate_size(self, size):
-        """
-        validates the size, checking for errors
-        """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            return True
-        return False
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
