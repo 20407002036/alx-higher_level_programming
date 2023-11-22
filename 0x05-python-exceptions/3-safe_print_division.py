17b5ab7067d8613d07fb115fe3671d8cf63fdc7f
Solomon <kyobenimaru001@gmail.com>
2023-11-22 17:53:52 +0300

    commit for the file 2-safe

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,7 +8,7 @@ def safe_print_division(a, b):
     """
     try:
         res = a / b
-    except:
+    except Exception:
         res = None
     finally:
         print("Inside result: {}".format(res))
