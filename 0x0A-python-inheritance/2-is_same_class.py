42e6ef62754e04498ee9d73112c03c24a2269830
Solomon <kyobenimaru001@gmail.com>
2023-12-04 14:25:20 +0300

    commit for the 2-is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Fuctioon Def
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Defines the function to check if object is of the instance
+    class
+    """
+    if (type(obj) == a_class):
+        return True
+    else:
+        return False
