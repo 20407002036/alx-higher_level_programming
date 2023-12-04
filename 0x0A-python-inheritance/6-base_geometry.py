fb5bb04e3adee460940cdd09f009c3b68e348136
Solomon <kyobenimaru001@gmail.com>
2023-12-04 15:36:21 +0300

    commit for the file 6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+For the class Definition
+"""
+
+
+class BaseGeometry:
+    """
+    The class with No attributes
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
