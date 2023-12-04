cd031ba6d9260302abcf1d96bdb2ee02a204b899
Solomon <kyobenimaru001@gmail.com>
2023-12-04 16:20:03 +0300

    commit for the file 7-base correction

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
         Function that checks the input values
         """
         if (type(value) != int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(name +" must be an integer")
 
         if (value <= 0):
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(name +" must be greater than 0")
