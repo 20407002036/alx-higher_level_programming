43ae9c4fb244e21273b0ad04813f9fa9e3505e76
Solomon <kyobenimaru001@gmail.com>
2023-12-04 13:19:33 +0300

    commit for the file 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,8 @@
 Returns a list of available attributes and methods of an object
 """
 
+
 def lookup(obj):
+    """Lookup function 
+    """
     return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or callable(getattr(obj, attr))]
