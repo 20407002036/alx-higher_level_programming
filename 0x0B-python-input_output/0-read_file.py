db55d0196adaf0a6e926fa4bf632f4a8cb68acc1
Solomon <kyobenimaru001@gmail.com>
2023-12-05 13:54:00 +0300

    commit for the file 0-read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The Last exciting project
+"""
+
+
+def read_file(filename=""):
+    """
+    Enable for the file access
+    """
+
+    with open(filename, encoding = "utf-8") as f:
+        read_file = f.read()
+
+        print(read_file)
+
+    f.closed
