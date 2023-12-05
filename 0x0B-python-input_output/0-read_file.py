4a95ccfe23c918def96d9ac0692e5764f0c8ea5a
Solomon <kyobenimaru001@gmail.com>
2023-12-05 14:40:02 +0300

    commit for the file 0-read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,7 +9,7 @@ def read_file(filename=""):
     Enable for the file access
     """
 
-    with open(filename, encoding = "utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_file = f.read()
 
         print(read_file)
