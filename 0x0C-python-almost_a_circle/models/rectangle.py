34b6a7233fe93bb88c75071d6b499b7f7f1e271c
Solomon <kyobenimaru001@gmail.com>
2023-12-11 19:00:26 +0300

    commit for th modulke square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -151,3 +151,65 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """returns dictionary representation of current rectangle
+        """
+        return {'id': self.id,
+                'width': self.width,
+                'height': self.height,
+                'x': self.x,
+                'y': self.y}
+
+    def to_csv(self):
+        """returns a list containing csv representation of rectangle
+        """
+        return [self.id, self.width, self.height, self.x, self.y]
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,8 +23,7 @@ class Square(Rectangle):
         Eg [Rectabgle] (12) 2/1 - 4/6
         """
 
-        builder = "[Square] ({}) {}/{} - {}".format(self.__class__.__name__,
-                                                   self.id,
+        builder = "[Square] ({}) {}/{} - {}".format(self.id,
                                                    self.x,
                                                    self.y,
                                                    self.width)
@@ -40,5 +39,54 @@ class Square(Rectangle):
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+        self.width = value
+        self.height = value
+
+
+    def update(self, *args, **kwargs):
+        """
+        Has both a Dict and list
+        """
+        if args is not None and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        elif kwargs is not None:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """gets the dictionary representation of a square object
+        """
+        new_d = super().to_dictionary()
+        del new_d['height']
+        del new_d['width']
+        new_d['size'] = self.size
+        return new_d
+
+    def to_csv(self):
+        """returns a list containing csv representation of rectangle
+        """
+        return [self.id, self.size, self.x, self.y]
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
         self.width = value
         self.height = value
