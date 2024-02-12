2a72bb0eaf6d4b7307c7049f1b2378e7b6262671
Solomon <kyobenimaru001@gmail.com>
2024-02-12 22:17:01 +0300

    commt for the 0-js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arg = process.argv - 2;
+
+if (arg !== 0) {
+    console.log(process.argv[3]);
+}
+else
+    console.log('No arguement');
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const array[4] = process.argv;
+
+console.log(array[0] + ' is ' + array[1]);
