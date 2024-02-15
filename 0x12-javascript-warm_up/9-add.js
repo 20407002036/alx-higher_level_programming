599a6b111c8eef7b559a99d2bc0d8697646c3e56
Solomon <kyobenimaru001@gmail.com>
2024-02-15 12:07:01 +0300

    commit for the ddition function 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  const sum = a + b;
-  console.log(b);
+  return (a +b);
+}
+// The function receive the args as strings
+// Remember to parse them to int
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (isNaN(arg1) || (isNaN(arg2))) {
+    console.log('NaN');
+} else {
+    console.log(add(arg1, arg2));
 }
