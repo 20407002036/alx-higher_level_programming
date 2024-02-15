af654c04c59eadd67f2dc6b8e220fe3e6171e055
Solomon <kyobenimaru001@gmail.com>
2024-02-15 08:25:56 +0300

    2-arguements update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,12 +3,9 @@
 const array = process.argv.length - 2;
 
 if (array === 0) {
-  console.log("No arguement");
- }
-else if ( array === 1) {
-  console.log("Arguement found");
- }
-else
-    console.log("Arguments found");
-	       
-
+  console.log('No arguement');
+} else if (array === 1) {
+  console.log('Arguement found');
+} else {
+  console.log('Arguments found');
+}
