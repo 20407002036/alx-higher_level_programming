417880580ce7a1b6c86ef7d1858f67e890cc82a0
Solomon <kyobenimaru001@gmail.com>
2024-02-15 12:00:06 +0300

    commit for the 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+const intSize = parseInt(size);
+let i = 0, j = 0;
+
+if (isNaN(size) || isNaN(intSize)) {
+    console.log('Missing size');
+} else {
+    // The console.log function will print in the next line
+    
+    // for (i = 0; i < intSize; i++) {
+    //     for (j = 0; j < intSize; j++) {
+    //         console.log('X');
+    //     }
+    // }
+
+    // To solve construct  each row with the chars.
+    // print the row using console.log
+
+    for (i = 0; i < intSize; i++) {
+        // craete an empty string
+        let row = '';
+        for (j = 0; j < intSize; j++) {
+            // Append to the empty string.
+            row += 'X';
+        }
+
+        console.log(row);
+    }
+}
