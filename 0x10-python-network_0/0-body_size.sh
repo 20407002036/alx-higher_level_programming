3e806e2f835b1061d5c42348387140f4e6dc4cbc
Solomon <kyobenimaru001@gmail.com>
2024-03-19 22:02:43 +0300

    commit for the 0-body_size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+# It takes a URL, sends a request to that URL, displays ize of the body of the response
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request to the URL and store the response in a variable
+response=$(curl -sI "$1")
+
+# Extract the content-length header from the response and display the size in bytes
+
+size=$(echo -n "$response" | wc -c)
+
+echo "$content_length"
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,5 @@
+# By Solomon Kaniaru
+
+## 0x10. Python-Network#0
+
+This file will hold the files related to networks in Python, Bash All related to The Back-end, API
