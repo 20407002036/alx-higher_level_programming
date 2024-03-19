0c5e5086d785caee6eb09b6d49fb808173e30acc
Solomon <kyobenimaru001@gmail.com>
2024-03-19 22:54:59 +0300

    commit for the check reposnse

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request to the URL and store the response in a variable
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+
+# Check if the response code is 200 (OK)
+if [ "$response" -eq 200 ]; then
+    # Send a GET request to the URL and display the body of the response
+    curl -s "$1"
+else
+    echo "Response code is not 200 (OK)"
+fi
